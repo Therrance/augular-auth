a097910a38909c655a519030df514e82edc12997
therrance <lozuvatka@gmail.com>
2016-11-25 15:25:55 +0000

    observe routeProvider api

--- file.js
+++ file.js
@@ -2,20 +2,25 @@
 
 angular.module('app', ['ngRoute'])
 
-/*.config(function($routeProvider) {
+.config(function($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'app.html',
         controller: 'AppCtrl',
         controllerAs: 'app'
+    })
+    .when('/cookies', {
+        template: 'mnom monm mnom'
+    }).otherwise({
+        template: 'this Route isn\'t set'
     });
-})*/
+})
 
 .controller('AppCtrl', function($route) {
-    $route.routes['/'] = {
+/*    $route.routes['/'] = {
         templateUrl: 'app.html',
         controller: 'AppCtrl',
         controllerAs: 'app'
-    };
+    };*/
 
     let self = this;
     self.message = 'The routing is working!';
