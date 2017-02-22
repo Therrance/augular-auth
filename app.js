b9b783c8bdbb0f4866e8c5302215fe74973b754f
therrance <lozuvatka@gmail.com>
2017-02-22 10:06:22 +0000

    Persisting tokens to localStorage

--- app.js
+++ app.js
@@ -49,6 +49,14 @@
       var base64 = base64Url.replace('-', '+').replace('_', '/');
       return JSON.parse($window.atob(base64));
     };
+    
+    self.saveToken = function(token){
+      $window.localStorage['jwtToken'] = token;
+    };
+    
+    self.getToken = function(){
+      return $window.localStorage['jwtToken'];
+    };
 
   }
 
